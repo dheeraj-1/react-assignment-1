dd73ea03c2b89378da8161e18b69e544cd4976f5
nagsalesforcedev <dgsalesforcedev@nagarro.com>
2020-07-30 13:46:14 +0530

    Updates

---
+++ precache-manifest.39ce66ec4dc9089499e4e5ac0e56f4fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a490e52a26a69ad1d7dc49c9fbc54d",
+    "url": "/react-assignment-1/index.html"
+  },
+  {
+    "revision": "92ff7a10475d614e7370",
+    "url": "/react-assignment-1/static/css/main.e0e31524.chunk.css"
+  },
+  {
+    "revision": "2b2930ca95faf7a060c8",
+    "url": "/react-assignment-1/static/js/2.2f0026d7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-assignment-1/static/js/2.2f0026d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ff7a10475d614e7370",
+    "url": "/react-assignment-1/static/js/main.3b3d9925.chunk.js"
+  },
+  {
+    "revision": "e073dabc79f0d9d6e3be",
+    "url": "/react-assignment-1/static/js/runtime-main.0b016843.js"
+  }
+]);
